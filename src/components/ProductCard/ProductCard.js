ac785315ee78fbc2e5d4ba7168c6ee8e4d80bf28
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-12-17 14:28:43 +0530

    update delete and edit button css

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,45 +1,51 @@
-import React from 'react';
+import React from "react";
 import "./ProductCard.css";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
+import IconEdit from "./../../assets/edit.png"
+import IconDelete from "./../../assets/delete.png"
 
-function ProductCard({item, price, quantity}) {
+function ProductCard({ item, price, quantity }) {
   const navigate = useNavigate();
 
-  const deleteProduct = async(item)=>{
-    const response =  await axios.delete(`https://store-backend-f2ej.onrender.com/products/${item}`);
+  const deleteProduct = async (item) => {
+    const response = await axios.delete(
+      `https://store-backend-f2ej.onrender.com/products/${item}`
+    );
     window.location.reload();
-  }
+  };
 
   return (
-      <div className='Product-card' onClick={(e)=>{
+    <div
+      className="Product-card"
+      onClick={(e) => {
         navigate(`/detail/${item}`);
-      }}> 
+      }}
+    >
+      <h3 className="Product-item">{item}</h3>
+      <div>
+        <span className="Product-price">Price: {price}/-</span>
+        <span className="Product-quantity">Quantity: {quantity}</span>
+      </div>
 
-        <h3 className='Product-item'>{item}</h3>
-        <div>
-          <span className='Product-price'>Price: {price}/-</span>
-          <span className='Product-quantity'>Quantity: {quantity}</span>
-        </div>
-
-        <span className='btn-delete' 
-          onClick={(e) => {
+      <span
+        onClick={(e) => {
           e.stopPropagation();
           deleteProduct(item);
-          }}
-          >
-            Delete
-        </span>
+        }}
+      >
+        <img src={IconDelete} alt="Delete" className="btn-delete"/>
+      </span>
 
-        <span className='btn-edit' 
-          onClick={(e) => {
+      <span
+        onClick={(e) => {
           e.stopPropagation();
           navigate(`/edit/${item}`);
-          }}
-          >
-            Edit
-        </span>
-      </div>
+        }}
+      >
+        <img src={IconEdit} alt="Edit"  className="btn-edit"/>
+      </span>
+    </div>
   );
 }
 
